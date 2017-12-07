57b412c40214c3fe75a7e90b671565b21c2217f3
seonfang <seonfang@qq.com>
2017-12-07 21:02:48 +0800

    整理笔记

--- src/main/java/com/example/contro/ControllerTest.java
+++ src/main/java/com/example/contro/ControllerTest.java
@@ -6,7 +6,6 @@ import javax.security.auth.message.callback.PrivateKeyCallback.Request;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 
 import com.example.service.UserService;
 
